be545a256cc593f40efc50f2f946ddcfca3d738a
Mwagiru <joymwagiru@gmail.com>
2022-03-27 07:38:51 +0300

    Halfway done

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution here!
+ const cats=["Milo", "Otis","Garfield"];
+ function destructivelyAppendCat(name){
+     cats.push("Ralph");
+     console.log(cats);
+ }
+ destructivelyAppendCat();
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+    console.log(cats);
+}
+destructivelyPrependCat();
+//
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    console.log(cats);
+}
+destructivelyRemoveLastCat();
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    console.log(cats);
+}
+destructivelyRemoveFirstCat();
+function appendCat(name){
+    const lotsaCats = [...cats, "Broom"];
+    console.log(lotsaCats);
+}
+appendCat();
+
+
+
+  
